3c0423f715e23417b706916b7b07358559296915
Aaron Longwell <a@adl.io>
2018-07-30 15:45:51 -0700

    Adds UniqueArtistIDs() to ArtistAliasCollection.

--- artist-alias.go
+++ artist-alias.go
@@ -13,6 +13,18 @@ type ArtistAlias struct {
 // ArtistAliasCollection is an alias for a slice of ArtistAlias
 type ArtistAliasCollection []*ArtistAlias
 
+func (c ArtistAliasCollection) UniqueArtistIDs() []int64 {
+	idMap := make(map[int64]bool)
+	for _, a := range c {
+		idMap[a.ArtistID] = true
+	}
+	ids := make([]int64, 0, len(idMap))
+	for id := range idMap {
+		ids = append(ids, id)
+	}
+	return ids
+}
+
 // FindArtistAliases retrieves a slice of ArtistAlias based on a dynamic query
 //
 func FindArtistAliases(db DB, clauses ...QueryFunc) (aliases ArtistAliasCollection, err error) {
